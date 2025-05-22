2117cd938077f66d31ffe4989c40a2024887175a
Abhirammm2208 <abhiram.chintalgat@gmail.com>
2025-05-23 00:29:33 +0530

    Updated Test 1 file

--- Cognizant Digital Nurture/2026_Cognizant Digital Nurture_Test 1/first repeated element in array.java
+++ Cognizant Digital Nurture/2026_Cognizant Digital Nurture_Test 1/first repeated element in array.java
@@ -7,61 +7,32 @@ in the array which is repeated.
 import java.util.*;
 
 class Main
-
 {
-
 	static void printFirstRepeating(int arr[])
-
 	{
-
 		int min = -1;
-
 		HashSet<Integer> set = new HashSet<>();
-
 		for (int i = arr.length - 1; i >= 0; i--)
-
 		{
-
 			if (set.contains(arr[i]))
-
 				min = i;
-
 			else
-
 				set.add(arr[i]);
-
 		}
-
 		if (min != -1)
-
 			System.out.println("The first repeating element is " + arr[min]);
-
 		else
-
 			System.out.println("There are no repeating elements");
-
 	}
-
 	public static void main(String[] args) throws java.lang.Exception
-
 	{
-
 		int i, n;
-
 		Scanner sc = new Scanner(System.in);
-
 		n = sc.nextInt();
-
 		int[] arr = new int[n];
-
 		for (i = 0; i < n; i++) {
-
 			arr[i] = sc.nextInt();
-
 		}
-
 		printFirstRepeating(arr);
-
 	}
-
 }
